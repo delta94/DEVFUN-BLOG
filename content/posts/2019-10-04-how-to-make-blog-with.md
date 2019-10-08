211ef518eeb219c5f4c6fbe86f82a1a5b47c324a
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-10-08 15:49:05 +0700

    Update env

--- content/posts/2019-10-04-how-to-make-blog-with.md
+++ content/posts/2019-10-04-how-to-make-blog-with.md
@@ -5,6 +5,6 @@ name: Ni
 date: 2019-10-04T04:04:50.289Z
 title: HOW TO MAKE BLOG WITH
 image: >-
-  https://res.cloudinary.com/coders-tokyo-shyn/image/upload/v1569817664/blog/banner-shopee-sieu-sale_mp3lt1.png
+  https://res.cloudinary.com/coders-tokyo-shyn/image/upload/q_auto/v1569817664/blog/banner-shopee-sieu-sale_mp3lt1.png
 ---
 HOW TO MAKE BLOG WITH
--- src/NetlifyCMS/index.jsx
+++ src/NetlifyCMS/index.jsx
@@ -2,6 +2,7 @@ import CMS from 'netlify-cms-app';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 import EditorYoutube from './components/EditorYoutube';
 import AnotherYoutube from './components/AnotherYoutube';
+import config from './config.json';
 // import BlogPostPreview from './previewTemplates/BlogPostPreview';
 import './widgets/Upload';
 // Media Libary
@@ -10,6 +11,16 @@ CMS.registerMediaLibrary(cloudinary);
 // Component Editor
 CMS.registerEditorComponent(EditorYoutube);
 CMS.registerEditorComponent(AnotherYoutube);
+CMS.init({
+  media_library: {
+    name: 'cloudinary',
+    config: {
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.CLOUD_API_KEY,
+    },
+  },
+  ...config,
+});
 
 // Preview Template
 
