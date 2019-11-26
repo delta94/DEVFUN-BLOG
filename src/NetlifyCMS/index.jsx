dcf4ebb2d38a35e177134e4922f56d086b499e26
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-11-26 11:58:17 +0700

    Change extension config

--- src/NetlifyCMS/index.jsx
+++ src/NetlifyCMS/index.jsx
@@ -2,26 +2,8 @@ import CMS from 'netlify-cms-app';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 import EditorYoutube from './components/EditorYoutube';
 import AnotherYoutube from './components/AnotherYoutube';
-import config from '../../static/admin/config.json';
-// import BlogPostPreview from './previewTemplates/BlogPostPreview';
 import './widgets/Upload';
-// Media Libary
-CMS.registerMediaLibrary(cloudinary);
 
-// Component Editor
+CMS.registerMediaLibrary(cloudinary);
 CMS.registerEditorComponent(EditorYoutube);
 CMS.registerEditorComponent(AnotherYoutube);
-CMS.init({
-  media_library: {
-    name: 'cloudinary',
-    config: {
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.CLOUD_API_KEY,
-    },
-  },
-  ...config,
-});
-
-// Preview Template
-
-// CMS.registerPreviewTemplate('blog', BlogPostPreview);
