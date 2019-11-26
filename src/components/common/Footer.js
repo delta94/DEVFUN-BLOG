ea26f02ed52f87a179a02627f36cc0069c4c6469
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-11-26 11:43:22 +0700

    Update version

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,22 +2,41 @@ import React from 'react';
 import tw from 'tailwind.macro';
 import styled from 'styled-components';
 
-const Text = styled.footer`
-  ${tw`text-center text-green-400 m-1 sm:rotate-270 md:rotate-0`}
+// const Text = styled.footer`
+//   ${tw`text-center text-green-400 m-1 sm:rotate-270 md:rotate-0`}
+// `;
+// const Input = styled.input`
+//   ${tw`bg-gray-200 focus:bg-white border-transparent focus:border-blue-400`}
+// `;
+const StyledFooter = styled.div``;
+
+StyledFooter.Ul = styled.ul`
+  ${tw`flex justify-between flex-col md:flex-row`}
 `;
-const Input = styled.input`
-  ${tw`bg-gray-200 focus:bg-white border-transparent focus:border-blue-400`}
+
+StyledFooter.Li = styled.li`
+  border: 1px solid white;
+  ${tw`text-center`}
 `;
-const Wrapper = styled.div`
-  ${tw`grid`}
+
+StyledFooter.Bottom = styled.section`
+  max-width: calc(100% - 120px);
+  border-top: 1px solid white;
+  ${tw`text-center m-auto p-3`}
 `;
 
 const Footer = () => {
   return (
-    <Wrapper>
-      <Text> Footer</Text>
-      <Input placeholder="Focus me" />
-    </Wrapper>
+    <StyledFooter>
+      <StyledFooter.Ul>
+        <StyledFooter.Li> 1</StyledFooter.Li>
+        <StyledFooter.Li> 1</StyledFooter.Li>
+        <StyledFooter.Li> 1</StyledFooter.Li>
+        <StyledFooter.Li> 1</StyledFooter.Li>
+        <StyledFooter.Li> 1</StyledFooter.Li>
+      </StyledFooter.Ul>
+      <StyledFooter.Bottom>Made with ❤ by Shyn</StyledFooter.Bottom>
+    </StyledFooter>
   );
 };
 
